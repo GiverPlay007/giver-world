909fe7abdd578b481e2d7137b16df6f9c6a4e618
GiverPlay007 <play.giver.007@gmail.com>
2021-04-27 13:05:54 -0300

    Basic player movement

--- main.asm
+++ main.asm
@@ -27,6 +27,41 @@ main PROC
 		; Exit game if user types x
 		cmp inputChar, "x"
 		je exitGame
+
+		cmp inputChar, "w"
+		je moveUp
+
+		cmp inputChar, "s"
+		je moveDown
+
+		cmp inputChar, "a"
+		je moveLeft
+
+		cmp inputChar, "d"
+		je moveRight
+
+		jmp gameLoop
+
+		moveUp:
+			dec yPos
+			call DrawPlayer
+			jmp gameLoop
+
+		moveDown:
+			inc yPos
+			call DrawPlayer
+			jmp gameLoop
+
+		moveLeft:
+			dec xPos
+			call DrawPlayer
+			jmp gameLoop
+
+		moveRight:
+			inc xPos
+			call DrawPlayer
+			jmp gameLoop
+
 	jmp gameLoop
 
 	exitGame:
