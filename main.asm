6f1401f00f3de78d523141a1d3aab7464f5409b5
GiverPlay007 <play.giver.007@gmail.com>
2021-04-27 12:45:10 -0300

    Draw player

--- main.asm
+++ main.asm
@@ -3,17 +3,32 @@ INCLUDE Irvine32.inc
 .data
 ground BYTE "------------------------------------------------------------------------------------------------------------------------", 0
 
+xPos BYTE 16
+yPos BYTE 10
+
 .code
 main PROC
-	; draw ground (0, 29)
+	; Draw ground (0, 29)
 	mov dl, 0
 	mov dh, 29
 	call Gotoxy
 	mov edx, OFFSET ground
 	call WriteString
 
+	call DrawPlayer
+
 	call ReadChar
 	exit
 main ENDP
+
+DrawPlayer PROC
+	; Draw player (xPos, yPos)
+	mov dl, xPos
+	mov dh, yPos
+	call Gotoxy
+	mov al, "X"
+	call WriteChar
+	ret
+DrawPlayer ENDP
 
 END main
